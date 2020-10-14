13f1dd882be15f5db0fe3472d2b373e97585b9e6
luqqy <you@example.com>
2020-10-14 11:51:50 +0300

    added views .py files

--- gallery/gallery/models.py
+++ gallery/gallery/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class gallery(models.Model):
     title=models.CharField(max_length=100)
-    desc=models.CharField(max_lenght=500)
+    description=models.CharField(max_length=500)
     image=models.ImageField(upload_to='image/')
       
